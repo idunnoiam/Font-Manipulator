6ed19c62960908b1693f97155d16e9fc2d574132
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2022-02-18 21:16:52 -0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,6 +15,10 @@ function setup() {
 
 function draw() {
     background('#969A97');
+    fill("#F90093");
+    stroke('#F90093');
+    text('Bill', 50, 400);
+    textSize(difference);
 }
 
 function modelLoaded() {
@@ -20,5 +28,12 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+
     }
 }
